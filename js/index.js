1ad56e418709ffae8d80f171f92cd93f477b81a3
Nehuen Colman <nehuencolman@gmail.com>
2024-09-20 20:09:22 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,10 @@
+alert("amo a mi novia")
+console.log("hola")
+console.error("error")
+let X = 9
+const Y = 6
+let nombre = necapojaja
+let edad = 31
+console.log("bienvenido" + nombre + "edad" + edad)
+console.log(`bienvenute ${nombre} edad ${edad}`)
+const NU = prompt(sus)
