aa96092831c259067670f29d58634e9c4efd9dda
Nehuen Colman <nehuencolman@gmail.com>
2024-09-27 20:05:33 -0300

    dia2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
-alert("amo a mi novia")
-console.log("hola")
-console.error("error")
-let X = 9
-const Y = 6
-let nombre = necapojaja
-let edad = 31
-console.log("bienvenido" + nombre + "edad" + edad)
-console.log(`bienvenute ${nombre} edad ${edad}`)
-const NU = prompt(sus)
+// alert("amo a mi novia")
+// console.log("hola")
+// console.error("error")
+// let X = 9
+// const Y = 6
+// let nombre = necapojaja
+// let edad = 31
+// console.log("bienvenido" + nombre + "edad" + edad)
+// console.log(`bienvenute ${nombre} edad ${edad}`)
+// let condicion = true
+// let condicion2 = false 
+// if (condicion) {
+//     alert("Correcto!")
+// }
+// else if(condicion2) {
+//     alert("Falso!")
+// }
+// else {
+//     alert("Ninguna")
+// } 
+// const num1 = 15;
+// if(num1 >= 18) {
+//   alert("Pasa bro")  
+// }
+// else if(num1 > 60) {
+//     alert("Viejo verde")
+// }
+// else{
+//     alert("Afuera peque")
+// }
